b8ef481d15960eb308bf611f0c35196b5a3f5e87
guilhem <g;raux@icloud.com>
2021-05-13 23:21:14 +0200

    added the Stats class to the project

---
+++ Stats.py
@@ -0,0 +1,28 @@
+class Stats:
+
+    values: dict = dict([("strength", 0), ("attack", 0), ("magic", 0),
+                         ("agility", 0), ("defence", 0)])
+
+    def __init__(self, stat_list):
+        if len(self.values) != len(stat_list):
+            raise ValueError(
+                "the length of the stat_list argument must be equal to the number of stats"
+            )
+        keys = list(self.values)
+        for i in range(len(stat_list)):
+            self.values[keys[i]] = stat_list[i]
+
+    def get_strength(self) -> int:
+        return self.values["strength"]
+
+    def get_attack(self) -> int:
+        return self.values["attack"]
+
+    def get_magic(self) -> int:
+        return self.values["magic"]
+
+    def get_agility(self) -> int:
+        return self.values["agility"]
+
+    def get_defense(self) -> int:
+        return self.values["defense"]
