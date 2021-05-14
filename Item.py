458353c2039c9c44b89d14e79b4d91c22c7aa2dc
marin <marin.cohu@gmail.com>
2021-05-14 19:39:57 +0200

    les constructeurs des classes Weapon et Armor sont finis (sauf erreur)

--- Item.py
+++ Item.py
@@ -14,3 +14,33 @@ class Item:
         self.name = name
         self.stats = stats
         self.rarity = rarity
+
+
+class Weapon(Item) :
+
+    damage : float
+    mag_ratio : float
+    phys_ratio : float
+
+    def __init__(self, name : str, stats : Stats, rarity : Rarity, damage : double, mag_ratio : double, phys_ratio : double):
+        self.damage = damage
+        self.mag_ratio = mag_ratio
+        self.phys_ratio = phys_ratio
+
+        super(Weapon, self).__init__(str, stats, rarity)
+
+
+class Armor(Item) :
+
+    armor : float
+    mag_ratio : float
+    phys_ratio : float
+
+    def __init__(self, name : str, stats : Stats, rarity : Rarity, armor : float, mag_ratio : double, phys_ratio : double):
+
+        self.armor = armor
+        self.mag_ratio = mag_ratio
+        self.phys_ratio = phys_ratio
+
+        super(Armor, self).__init__(str, stats, rarity)
+
