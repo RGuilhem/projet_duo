f24725c8a5c013ea4c30ce0bc1245ec409180b9d
guilhem <g;raux@icloud.com>
2021-05-14 15:07:40 +0200

    refactoring

--- Entity.py
+++ Entity.py
@@ -1,24 +1,24 @@
-from Stats import *
+from Stats import Stats
 
-class Entity :
+
+class Entity:
 
     # Attributes
-    name : str
-    hp : int
-    lvl : int
-    base_stats : Stats
-    total_stats : Stats
-    #stuff : List[Item]
+    name: str
+    hp: int
+    lvl: int
+    base_stats: Stats
+    total_stats: Stats
+    #stuff: List[Item]
     position = [0] * 2
 
     # Methods
-    def __init__(self, name : str, base_stats : Stats):
+    def __init__(self, name: str, base_stats: Stats):
         self.name = name
         self.base_stats = base_stats
         #self.stuff = stuff
         self.hp = self.compute_hp(base_stats)
         self.lvl = 0
-
 
     def do_damage(self, target):
         pass
@@ -27,9 +27,8 @@ class Entity :
         pass
 
     @staticmethod
-    def compute_hp(player_stats : Stats):
+    def compute_hp(player_stats: Stats):
         pass
 
-    def compute_total_stats(self, base_stats : Stats) :
+    def compute_total_stats(self, base_stats: Stats):
         pass
-
--- Player.py
+++ Player.py
@@ -1,8 +1,9 @@
-from Entity import *
-from Stats import *
-from Item import *
+from Entity import Entity
+from Stats import Stats
+from Item import Item
 
-class Player(Entity) :
+
+class Player(Entity):
 
     #Pre-existing attributes
     """
@@ -22,18 +23,18 @@ class Player(Entity) :
     # Methods
     def __init__(self, player_name: str, base_stats: Stats):
         super().__init__(player_name, base_stats)
-
+        self.position = [0, 0]
 
     @staticmethod
-    def move(self, x : int, y : int) :
+    def move(self, x: int, y: int):
         self.position[0] += x
         self.position[1] += y
-        return position
+
 
 def main():
 
     obj = Player("Sylver", Stats([1, 2, 3, 4, 5]))
     print(obj.lvl, obj.position, obj.name)
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
