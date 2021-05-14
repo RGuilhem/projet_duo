b327ebeb04754014b45403ebb4dd74971baf4a2a
guilhem <g;raux@icloud.com>
2021-05-14 22:35:36 +0200

    changed range from 1->5 to 0->4 to be used as indexes

--- RarityEnum.py
+++ RarityEnum.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class Rarity(Enum):
-    NORMAL: 1
-    MAGICAL: 2
-    RARE: 3
-    EPIC: 4
-    LEGENDARY: 5
+    NORMAL: 0
+    MAGICAL: 1
+    RARE: 2
+    EPIC: 3
+    LEGENDARY: 4
